febe2af17f4edeb7e2d41b66ed97ebc58d0becab
S M Rishad <rishad.black@gmail.com>
2026-01-11 06:22:23 +0600

    init

---
+++ index.php
@@ -0,0 +1,385 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+
+    <!-- Page Title-->
+    <title>Dream Zone Agro Food</title>
+
+    <!-- Meta Tags-->
+    <meta name="description" content="Dream Zone Agro Food">
+    <meta name="keywords" content="Dream Zone Agro Food">
+    <meta name="author" content="mix_design">
+
+    <!-- Viewport Meta-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Template Favicon & Icons Start -->
+    <link rel="icon" href="img/favicon/favicon.png" sizes="any">
+    <link rel="icon" href="img/favicon/favicon.png" type="image/svg+xml">
+    <link rel="apple-touch-icon" href="img/favicon/favicon.png">
+    <link rel="manifest" href="img/favicon/color-1/manifest.webmanifest">
+    <!-- Template Favicon & Icons End -->
+
+    <!-- Facebook Metadata Start -->
+    <meta property="og:image:height" content="1200">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:title" content="Dream Zone Agro Food">
+    <meta property="og:description" content="Dream Zone Agro Food">
+    <meta property="og:url" content="index.html">
+    <meta property="og:image" content="img/favicon/favicon.png">
+    <!-- Facebook Metadata End -->
+
+    <!-- Template Styles Start -->
+    <link rel="stylesheet" type="text/css" href="css/loaders/loader-color-1.css">
+    <link rel="stylesheet" type="text/css" href="css/plugins.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/colors/color-1.css">
+    <!-- Template Styles End -->
+
+    <!-- Custom Browser Color Start -->
+    <meta name="theme-color" content="#B247B2">
+    <meta name="msapplication-navbutton-color" content="#B247B2">
+    <meta name="apple-mobile-web-app-status-bar-style" content="#B247B2">
+    <!-- Custom Browser Color End -->
+
+</head>
+
+<body>
+
+    <!-- Loader Start -->
+    <div class="loader">
+        <div class="loader__content">
+            <div class="loader__logo slideInDown">
+                <!-- Your Logo Here -->
+                <img src="img/logo.png" alt="Dream Zone Agro Food">
+            </div>
+            <div class="loader__caption slideInUp">
+                <p class="loading-text">Loading</p>
+                <span class="loading-dots">
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                    <span class="dot"></span>
+                </span>
+            </div>
+        </div>
+    </div>
+    <!-- Loader End -->
+
+    <!-- Custom HTML Start -->
+
+    <!-- Main Section Start-->
+    <section id="main" class="section-main">
+        <div class="container-fluid p-0 fullheight">
+            <div class="row g-0 fullheight">
+
+                <!-- Main Section Intro Start -->
+                <div id="intro" class="col-12 col-xl-9 section-main__intro">
+
+                    <!-- Intro Media Start -->
+                    <div class="intro__media">
+
+                        <!-- Main Section Image Background Start -->
+                        <div class="media__image media-1"></div>
+                        <!-- Main Section Image Background End -->
+
+                        <!-- Main Section Color Layer Start -->
+                        <div class="color-layer color-layer-medium"></div>
+                        <!-- Main Section Color Layer End -->
+
+                    </div>
+                    <!-- Intro Media End -->
+
+                    <!-- Intro Header Start -->
+                    <div class="intro__header">
+
+                        <!-- Logo Start -->
+                        <div class="logo">
+                            <img src="img/logo.png" alt="Dream Zone Agro Food" height="10px">
+                        </div>
+                        <!-- Logo End -->
+
+                        <!-- Menu Trigger Start - Visible on Mobile -->
+                        <a href="#0" id="menu-trigger" class="menu-trigger">
+                            <i class="fa-solid fa-bars"></i>
+                        </a>
+                        <!-- Menu Trigger End - Visible on Mobile -->
+
+                    </div>
+                    <!-- Intro Header End -->
+
+                    <!-- Navigation Start -->
+                    <div id="menu" class="menu">
+
+                        <!-- Navigation Controls Start - Visible on Mobile -->
+                        <div class="menu__controls">
+                            <a href="#0" id="menu-close" class="menu-close section-close">
+                                <i class="fa-solid fa-xmark"></i>
+                            </a>
+                        </div>
+                        <!-- Navigation Controls End - Visible on Mobile -->
+
+                        <!-- Navigation Menu Start
+                        <div class="menu__container">
+                            <nav class="navigation">
+                                <ul>
+                                    <li class="transition-el transition-el-1"><a href="#" id="home-trigger"
+                                            class="home-trigger">Home</a></li>
+                                    <li class="transition-el transition-el-2"><a href="#" id="about-trigger"
+                                            class="inner-trigger">About us</a></li>
+                                    <li class="transition-el transition-el-3"><a href="#" id="works-trigger"
+                                            class="inner-trigger">Our works</a></li>
+                                    <li class="transition-el transition-el-4"><a href="#" id="contact-trigger"
+                                            class="inner-trigger">Contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                         Navigation Menu End -->
+
+                    </div>
+                    <!-- Navigation End -->
+
+                    <!-- Intro Headline Start -->
+                    <div class="intro__headline">
+                        <p class="headline__subtitle">We are</p>
+
+                        <!-- Animated Headline Start -->
+                        <h1 class="headline__title clip">
+                            <span class="words-wrapper">
+                                <b class="is-visible">Nature</b>
+                                <b>Trust</b>
+                                <b>Coming soon</b>
+                            </span>
+                        </h1>
+                        <!-- Animated Headline End -->
+
+                        <p class="headline__description">Dream Zone Agro Food is bringing something wholesome and
+                            exciting.
+                            Exclusive surprises for our members—stay connected.</p>
+                    </div>
+                    <!-- Intro Headline End -->
+
+                    <!-- Intro Countdown Start -->
+                    <div class="intro__countdown">
+                        <div class="countdown">
+                            <div id="countdown"></div>
+                            <span class="help-text">seconds<br>to launch</span>
+                        </div>
+                    </div>
+                    <!-- Intro Countdown End -->
+
+                    <!-- Intro Сopyright Start -->
+                    <div class="intro__copyright">
+                        <p class="copyright">All rights reserved &copy; 2026. Made with
+                            <i class="fa-solid fa-heart"></i>
+                        </p>
+                    </div>
+                    <!-- Intro Сopyright End -->
+
+                </div>
+                <!-- Main Section Intro End -->
+
+                <!-- Main Section Aside Start -->
+                <div class="col-12 col-xl-3 section-main__aside">
+
+                    <!-- Aside Content Start -->
+                    <div class="aside__content">
+                        <p class="aside__title">Our website is under construction, but we are ready to go!</p>
+                        <p class="aside__text">Want to know more about us, tell us about your project or just to say
+                            hello? Drop us a line and we will get back as soon as possible.</p>
+                        <a href="#0" id="stayintouch-trigger" class="button button-outline-light">
+                            <span class="button-caption">Write a line</span>
+                        </a>
+                    </div>
+                    <!-- Aside Content End -->
+
+                    <!-- Aside Socials Start -->
+                    <div class="aside__socials">
+                        <ul class="socials">
+                            <li>
+                                <a href="https://www.instagram.com/" target="_blank">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com/" target="_blank">
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://x.com/" target="_blank">
+                                    <i class="fa-brands fa-x"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                    <!-- Aside Socials End -->
+
+                    <!-- Stay-in-Touch Popup Start -->
+                    <div id="stayintouch" class="stayintouch">
+
+                        <!-- Stay-in-touch Controls Start -->
+                        <div class="stayintouch__controls">
+                            <a class="button-square button-outline-light section-close" href="#0"
+                                id="stayintouch-close">
+                                <i class="fa-solid fa-xmark"></i>
+                            </a>
+                        </div>
+                        <!-- Stay-in-touch Controls End -->
+
+                        <!-- Stay-in-touch Content Start -->
+                        <div class="stayintouch__content">
+
+                            <!-- Stay-in-touch Content Block Title Start -->
+                            <div class="content-block margin-bottom-s">
+                                <p class="popup__title light transition-el transition-el-1">Stay in touch</p>
+                            </div>
+                            <!-- Stay-in-touch Content Block Title End -->
+
+                            <!-- Stay-in-touch Content Block Form Start -->
+                            <div class="content-block grid-block">
+                                <div class="form-container">
+
+                                    <!-- Reply Messages Start -->
+                                    <div class="reply-group">
+                                        <i class="fa-solid fa-circle-check reply-group__icon light"></i>
+                                        <p class="reply-group__title light">Done!</p>
+                                        <span class="reply-group__text light">Thanks for your message. We will get back
+                                            as soon as possible.</span>
+                                    </div>
+                                    <!-- Reply Messages End -->
+
+                                    <!-- Stay-in-touch Form Start -->
+                                    <form class="form form-light stayintouch-form" id="stayintouch-form">
+                                        <!-- Hidden Required Fields -->
+                                        <input type="hidden" name="project_name" value="Axio Coming Soon Template">
+                                        <input type="hidden" name="admin_email" value="support@mixdesign.dev">
+                                        <input type="hidden" name="form_subject" value="Stay-in-touch Form Message">
+                                        <!-- END Hidden Required Fields-->
+                                        <div class="container-fluid px-4">
+                                            <div class="row gx-5">
+                                                <div class="col-12">
+                                                    <input class="transition-el transition-el-2" type="text" name="Name"
+                                                        placeholder="Your Name*" required>
+                                                </div>
+                                                <div class="col-12">
+                                                    <input class="transition-el transition-el-3" type="email"
+                                                        name="E-mail" placeholder="Email Adress*" required>
+                                                </div>
+                                                <div class="col-12">
+                                                    <textarea class="transition-el transition-el-4" name="Message"
+                                                        placeholder="Your Message*" required></textarea>
+                                                </div>
+                                                <div class="col-12">
+                                                    <span class="inputs-description opacity-el opacity-el-1">*Required
+                                                        fields</span>
+                                                    <button
+                                                        class="button button-solid-light transition-el transition-el-5">
+                                                        <span class="button-caption">Send</span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <!-- Stay-in-touch Form End -->
+
+                                </div>
+                            </div>
+                            <!-- Stay-in-touch Content Block Form End -->
+
+                        </div>
+                        <!-- Stay-in-touch Content End -->
+
+                    </div>
+                    <!-- Stay-in-Touch Popup End -->
+
+                </div>
+                <!-- Main Section Aside End -->
+
+            </div>
+        </div>
+    </section>
+    <!-- Main Section End -->
+
+    <!-- Custom HTML End -->
+
+    <!-- Root element of PhotoSwipe. Must have class pswp. -->
+    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+
+        <!-- Background of PhotoSwipe.
+      It's a separate element, as animating opacity is faster than rgba(). -->
+        <div class="pswp__bg"></div>
+
+        <!-- Slides wrapper with overflow:hidden. -->
+        <div class="pswp__scroll-wrap">
+
+            <!-- Container that holds slides. PhotoSwipe keeps only 3 slides in DOM to save memory. -->
+            <!-- don't modify these 3 pswp__item elements, data is added later on. -->
+            <div class="pswp__container">
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+            </div>
+
+            <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
+            <div class="pswp__ui pswp__ui--hidden">
+
+                <div class="pswp__top-bar">
+
+                    <!--  Controls are self-explanatory. Order can be changed. -->
+
+                    <div class="pswp__counter"></div>
+
+                    <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+
+                    <button class="pswp__button pswp__button--share" title="Share"></button>
+
+                    <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+
+                    <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+
+                    <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR -->
+                    <!-- element will get class pswp__preloader--active when preloader is running -->
+                    <div class="pswp__preloader">
+                        <div class="pswp__preloader__icn">
+                            <div class="pswp__preloader__cut">
+                                <div class="pswp__preloader__donut"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                    <div class="pswp__share-tooltip"></div>
+                </div>
+
+                <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
+                </button>
+
+                <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
+                </button>
+
+                <div class="pswp__caption">
+                    <div class="pswp__caption__center"></div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Load Scripts Start -->
+    <script src="js/libs.min.js"></script>
+    <script src="js/gallery-init.js"></script>
+    <script src="js/custom.js?v=111"></script>
+    <script src="js/maps/map-color-1.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDNMDtmEsvSevM4ztfsbhLfLNZhKHCvWXk"></script>
+    <!-- Load Scripts End -->
+
+</body>
+
+</html>
